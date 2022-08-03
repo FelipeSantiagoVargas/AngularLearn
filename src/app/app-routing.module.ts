0d15791b672cbfb9b4d377b9de7d6af1c1f646d2
FelipeVargas <FelipeVargas@software-genesis.com>
2022-08-03 17:56:28 -0500

    1:14:00

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BaseAuthComponent } from './presentation/layout/base-auth/base-auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'auth',
+  component: BaseAuthComponent,
+  children: [
+    {
+      path: 'login',
+      loadChildren: ()=>
+        import('./presentation/features/auth/login/login.module').then(
+          (m) => m.LoginModule
+        ),
+    },
+    {
+      path: 'signup',
+      loadChildren: ()=>
+        import('./presentation/features/auth/signup/signup.module').then(
+          (m) => m.SignupModule
+        ),
+    }
+  ],
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
